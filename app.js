f4fced3d0bab54762d3e68697d226dbf2d0126d4
ripan <reponmondal007@gmail.com>
2023-12-07 00:45:13 +0530

    file modified

---
+++ app.js
@@ -0,0 +1,13 @@
+import { Text, View } from 'react-native'
+
+function App() {
+  return (
+    <View>
+      <Text style={{ color: 'red' }}>
+        welcome to react native ,have all the fun ,jjjj gggggg hhhh
+      </Text>
+    </View>
+  )
+}
+
+export default App
---
+++ app/index.js
@@ -0,0 +1,13 @@
+import { Text, View } from 'react-native'
+
+function App() {
+  return (
+    <View>
+      <Text style={{ color: 'orange' }}>
+        welcome to react native ,have all the fun
+      </Text>
+    </View>
+  )
+}
+
+export default App
